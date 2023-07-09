162e2b4e652446feb1d8a87632538d0e32406046
NazariiIhnat <ignatnazar@gmail.com>
2023-07-09 23:07:39 +0200

    Add preloading strategy

--- angular/recipes/src/app/app.module.ts
+++ angular/recipes/src/app/app.module.ts
@@ -5,12 +5,8 @@ import { HeaderComponent } from './header/app.component';
 import { FormsModule } from '@angular/forms';
 import { RoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { RecipesModule } from './recipes/recipes.module';
-import { RecipesRoutingModule } from './recipes/recipes-routing.module';
-import { ShoppingListModule } from './shopping-list/shoping-list.module';
 import { CoreModule } from './core.module';
 import { SharedModule } from 'src/shared/shared.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
