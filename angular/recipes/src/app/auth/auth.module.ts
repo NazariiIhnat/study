af410bd6ce7c57ce4a7a157dc3633461032832e9
NazariiIhnat <ignatnazar@gmail.com>
2023-07-10 21:27:50 +0200

    Fixed bug with showing alert component when input incorrect  email or password

--- angular/recipes/src/app/auth/auth.module.ts
+++ angular/recipes/src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AlertDirective } from 'src/shared/alert/alert.directive';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/shared/shared.module';
 
 @NgModule({
   declarations: [AuthComponent],
@@ -11,6 +12,7 @@ import { CommonModule } from '@angular/common';
     RouterModule.forChild([{ path: '', component: AuthComponent }]),
     CommonModule,
     FormsModule,
+    SharedModule,
   ],
 })
 export class AuthModule {}
--- angular/recipes/src/shared/shared.module.ts
+++ angular/recipes/src/shared/shared.module.ts
@@ -3,15 +3,22 @@ import { AlertComponent } from './alert/alert.component';
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 import { CommonModule } from '@angular/common';
 import { DropdownDirective } from './dropdown.directive';
+import { AlertDirective } from './alert/alert.directive';
 
 @NgModule({
-  declarations: [AlertComponent, LoadingSpinnerComponent, DropdownDirective],
+  declarations: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    DropdownDirective,
+    AlertDirective,
+  ],
   imports: [CommonModule],
   exports: [
     AlertComponent,
     LoadingSpinnerComponent,
     DropdownDirective,
     CommonModule,
+    AlertDirective,
   ],
 })
 export class SharedModule {}
