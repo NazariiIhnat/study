21910ba6079715e527e8d4ab0f55378a146cb52c
NazariiIhnat <ignatnazar@gmail.com>
2023-07-13 12:12:20 +0200

    Implemented own createAction() and createReducer() functions (this code is commented)

--- angular/NgRx/src/app/store/counter.actions.ts
+++ angular/NgRx/src/app/store/counter.actions.ts
@@ -4,3 +4,12 @@ export const increment = createAction(
   '[Counter] Increment',
   props<{ value: number }>()
 );
+
+// export const INCREMENT = '[Counter] Increment';
+
+// export class IncrementAction implements Action {
+//   readonly type = '[Counter] Increment';
+//   constructor(public value: number) {}
+// }
+
+// export type CounterActions = IncrementAction;
--- angular/NgRx/src/app/store/counter.reducer.ts
+++ angular/NgRx/src/app/store/counter.reducer.ts
@@ -6,3 +6,12 @@ export const counterReducer = createReducer(
   initialState,
   on(increment, (state, action) => state + action.value)
 );
+
+// export function counterReducer(
+//   state = initialState,
+//   action: CounterActions | Action
+// ) {
+//   return action.type === INCREMENT
+//     ? state + (action as IncrementAction).value
+//     : state;
+// }
