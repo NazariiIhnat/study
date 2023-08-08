074877b6ad3dc66faffcb319fb4ac6270bc5b9ab
NazariiIhnat <ignatnazar@gmail.com>
2023-08-08 23:30:46 +0200

    Learned php controls

---
+++ php/control.php
@@ -0,0 +1,49 @@
+<?php
+
+// $values = [];
+// if(empty($values)){
+//   echo 'No values in array';
+// } else {
+//   echo 'Few values in array';
+// }
+
+// $value = 1;
+// while ($value <= 12){
+//   echo $value . ', ';
+//   $value++;
+// }
+
+// for($i = 0; $i <= 10; $i++) {
+//   echo $i . ", ";
+// }
+
+// $val = 232;
+// if($val < 10){
+//   echo 'one';
+// } elseif ($val >= 10 && $val < 100){
+//   echo 'two';
+// } elseif ($val >= 100 && $val <1000) {
+//   echo 'three';
+// } else{
+//   echo 'four';
+// }
+
+// $val = 23;
+// switch($val) {
+//   case $val < 10: echo 'one'; break;
+//   case $val >= 10 && $val < 100: echo 'two'; break;
+//   case $val >= 100 && $val < 1000: echo 'three'; break;
+//   default: echo 'four'; 
+// }
+
+//practice 
+
+// $array = [];
+// for ($i = 1; $i <= 10; $i++) {
+//     switch ($i) {
+//         case $i < 4: $array[$i] = "a"; break;
+//         case $i <= 7: $array[$i] = "b"; break;
+//         case $i > 7: $array[$i] = "c"; break;
+//     }
+// }
+
