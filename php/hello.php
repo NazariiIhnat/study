47b561294cc3258e398c00fa9a9a6cef6bb52ef8
NazariiIhnat <ignatnazar@gmail.com>
2023-08-08 22:14:03 +0200

    Learned php variables

--- php/hello.php
+++ php/hello.php
@@ -1,2 +1,40 @@
 <?php
-each "Hello world"
+// $message = 'Hello world';
+// $price = 3;
+// $value = 1.23;
+// echo $message;
+
+// var_dump($message);
+// var_dump($price);
+// var_dump($value);
+
+// $is_admin = true;
+// $user = null;
+// var_dump($is_admin, $user);
+
+// $price = 10;
+// $quantity = 12;
+// echo $price * $quantity;
+
+// $price = 12.4;
+// $quantity = 12;
+// var_dump($price*$quantity);
+
+// $name = 'Nazarii';
+// $surname = 'Ihnat';
+// echo $name . " " . $surname;
+
+// $is_editor = true;
+// $is_admin = false;
+// var_dump($is_editor && ! $is_admin);
+
+// $start = "at 3 o'clock";
+// $end = 'at 4 o\'clock';
+// echo $start . ' ' .$end;
+
+//variable practice 
+// $name = 'Nazarii';
+// $age = 29;
+// $status = true;
+// $new_age = $age + 10;
+// $sentence = "{$name} knows PHP!";
