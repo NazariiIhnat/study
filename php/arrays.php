6f46e57b36b87930a4758412148d3161a6f8c1d5
NazariiIhnat <ignatnazar@gmail.com>
2023-08-08 22:51:48 +0200

    Learned php arrays

---
+++ php/arrays.php
@@ -0,0 +1,66 @@
+<?php
+
+// $articless = ['First article', 'Second article', 'Third article'];
+// var_dump($articless[1]);
+
+// $articless = [4 => 'First article', 'test' => 'Second article', 'Third article'];
+// var_dump($articless['test']);
+
+// $values = [
+//   'name' => 'Nazarii',
+//   'surname' => 'Ihnat',
+//   'age' => 29,
+//   'city' => 'Wroclaw'
+// ];
+// var_dump($values);
+
+// $values = [[
+//     'name' => 'Nazarii',
+//     'surname' => 'Ihnat',
+//     'age' => 29,
+//     'city' => 'Wroclaw'
+// ],
+// [
+//   'name' => 'Khrystyna',
+//   'surname' => 'Haida',
+//   'age' => 26,
+//   'city' => 'Warsaw'
+// ]];
+// var_dump($values);
+// var_dump($values[0]['surname']);
+
+// $articless = ['First article', 'Second article', 'Third article'];
+// foreach($articless as $article) {
+//   echo "$article\n";
+// }
+
+// $values = [[
+//       'name' => 'Nazarii',
+//       'surname' => 'Ihnat',
+//       'age' => 29,
+//       'city' => 'Wroclaw'
+//   ],
+//   [
+//     'name' => 'Khrystyna',
+//     'surname' => 'Haida',
+//     'age' => 26,
+//     'city' => 'Warsaw'
+//   ]];
+
+// foreach($values as $value) {
+//   foreach($value as $test) {
+//     echo "{$test}. ";
+//   }
+// }
+
+// $articless = ['First article', 'test' => 'Second article', 'Third article'];
+// foreach($articless as $index => $article) {
+//   echo $index . ' - ' . $article . ', ';
+// }
+
+//array practice 
+
+// $array = ['One', 'Two', 'third' => 'Three'];
+// foreach ($array as $index => $value) {
+//     echo "{$index} = {$value}.";
+// }
