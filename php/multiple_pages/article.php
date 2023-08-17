d604c34f11be1f1b43c022898d3619dbba4c9d87
NazariiIhnat <ignatnazar@gmail.com>
2023-08-17 23:01:12 +0200

    Learned how to insert data to db

--- php/multiple_pages/article.php
+++ php/multiple_pages/article.php
@@ -1,37 +1,41 @@
 <?php
 
-include 'includes/database.php';
+require 'includes/database.php';
 
-if(isset($_GET['id']) && is_numeric($_GET['id'])){  
-  $sql = 
-  "SELECT *
-  FROM article
-  WHERE id = " . $_GET['id'];
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
 
-  $results = mysqli_query($conn, $sql);
-  if($results === false) {
-    echo mysqli_error($conn);
-  } else {
-    $article = mysqli_fetch_assoc($results);
-  }
+    $sql = "SELECT *
+            FROM article
+            WHERE id = " . $_GET['id'];
+
+    $results = mysqli_query($conn, $sql);
+
+    if ($results === false) {
+
+        echo mysqli_error($conn);
+
+    } else {
+
+        $article = mysqli_fetch_assoc($results);
+
+    }
+
 } else {
-  $article = null;
+    $article = null;
 }
+
 ?>
-
-
 <?php require 'includes/header.php'; ?>
-<ul>
-  <?php if($article === null): ?>
-  <p>No articles found</p>
-  <?php else :?>
-  <li>
-    <article>
-      <h2><?= $article['title']; ?> </h2>
-      <p> <?= $article['content']; ?> </p>
-      <p> <?= $article['published_at']; ?></p>
-    </article>
-  </li>
-</ul>
+
+<?php if ($article === null): ?>
+<p>Article not found.</p>
+<?php else: ?>
+
+<article>
+  <h2><?= $article['title']; ?></h2>
+  <p><?= $article['content']; ?></p>
+</article>
+
 <?php endif; ?>
+
 <?php require 'includes/footer.php'; ?>
