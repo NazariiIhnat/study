07e4476f2601615c5072e97f3d2ab67b06d37396
NazariiIhnat <ignatnazar@gmail.com>
2023-09-07 23:36:08 +0200

    Learn how to delete

--- php/multiple_pages/article.php
+++ php/multiple_pages/article.php
@@ -15,15 +15,17 @@ if (isset($_GET['id'])) {
 <?php require 'includes/header.php'; ?>
 
 <?php if ($article === null) : ?>
-    <p>Article not found.</p>
+<p>Article not found.</p>
 <?php else : ?>
 
-    <article>
-        <h2><?= htmlspecialchars($article['title']); ?></h2>
-        <p><?= htmlspecialchars($article['content']); ?></p>
-    </article>
+<article>
+  <h2><?= htmlspecialchars($article['title']); ?></h2>
+  <p><?= htmlspecialchars($article['content']); ?></p>
+</article>
 
-    <a href="edit-article.php?id=<?= $article['id']; ?>">Edit</a>
+<a href="edit-article.php?id=<?= $article['id']; ?>">Edit</a>
+<a href="delete-article.php?id=<?=$article['id']; ?>">Delete</a>
+
 
 <?php endif; ?>
 
