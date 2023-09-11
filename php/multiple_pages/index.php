9703f907dc0e39a3b6b312e311c5abd23ffeb0e8
NazariiIhnat <ignatnazar@gmail.com>
2023-09-11 19:35:31 +0200

    Add login, logout features. Also add sessions.

--- php/multiple_pages/index.php
+++ php/multiple_pages/index.php
@@ -1,6 +1,9 @@
 <?php
 
 require 'includes/database.php';
+require 'includes/auth.php';
+
+session_start();
 
 $conn = getDB();
 
@@ -19,22 +22,27 @@ if ($results === false) {
 ?>
 <?php require 'includes/header.php'; ?>
 
+<?php if (isLoggedIn()): ?>
+<p>You are logged in. <a href="logout.php">Log out</a></p>
 <a href="new-article.php">New article</a>
+<?php else: ?>
+<p>You are not logged in. <a href="login.php">Log in</a></p>
+<?php endif;?>
 
 <?php if (empty($articles)): ?>
-    <p>No articles found.</p>
+<p>No articles found.</p>
 <?php else: ?>
 
-    <ul>
-        <?php foreach ($articles as $article): ?>
-            <li>
-                <article>
-                    <h2><a href="article.php?id=<?= $article['id']; ?>"><?= htmlspecialchars($article['title']); ?></a></h2>
-                    <p><?= htmlspecialchars($article['content']); ?></p>
-                </article>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+<ul>
+  <?php foreach ($articles as $article): ?>
+  <li>
+    <article>
+      <h2><a href="article.php?id=<?= $article['id']; ?>"><?= htmlspecialchars($article['title']); ?></a></h2>
+      <p><?= htmlspecialchars($article['content']); ?></p>
+    </article>
+  </li>
+  <?php endforeach; ?>
+</ul>
 
 <?php endif; ?>
 
--- php/multiple_pages/new-article.php
+++ php/multiple_pages/new-article.php
@@ -3,6 +3,13 @@
 require 'includes/database.php';
 require 'includes/article.php';
 require 'includes/url.php';
+require 'includes/auth.php';
+
+session_start();
+
+if(! isLoggedIn()){
+    die('User is not logged in.');
+}
 
 $title = '';
 $content = '';
