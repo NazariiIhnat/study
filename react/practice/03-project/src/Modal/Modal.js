ad2c48fbe584d4fdf918ecdafc52eeca37511576
NazariiIhnat <ignatnazar@gmail.com>
2023-10-19 20:35:27 +0200

    Add portal to modal

--- react/practice/03-project/src/Modal/Modal.js
+++ react/practice/03-project/src/Modal/Modal.js
@@ -1,5 +1,20 @@
 import "./Modal.css";
-import { useState } from "react";
+import Wraper from "../Helpers/Wraper";
+import ReactDOM from "react-dom";
+
+const ModalPortal = (props) => {
+  return (
+    <div className="overlay" onClick={props.onClick}>
+      <div className="container">
+        <form>
+          <h2>Invalid input</h2>
+          <p>Please enter valid username and age (non-empty values)</p>
+          <button type="submit">Okay</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default function Modal(props) {
   const closeModal = (event) => {
@@ -8,14 +23,11 @@ export default function Modal(props) {
   };
 
   return (
-    <div className="overlay" onClick={closeModal}>
-      <div className="container">
-        <form onSubmit={closeModal}>
-          <h2>Invalid input</h2>
-          <p>Please enter valid username and age (non-empty values)</p>
-          <button type="submit">Okay</button>
-        </form>
-      </div>
-    </div>
+    <Wraper>
+      {ReactDOM.createPortal(
+        <ModalPortal onClick={closeModal} />,
+        document.getElementById("root-modal")
+      )}
+    </Wraper>
   );
 }
