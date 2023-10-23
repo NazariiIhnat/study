e3a930ae8ee9e2ad45aa6e8a04244a3e8bef3c36
NazariiIhnat <ignatnazar@gmail.com>
2023-10-23 23:45:30 +0200

    learned about useReducer()

--- react/practice/03-project/src/UserInput/UserInput.js
+++ react/practice/03-project/src/UserInput/UserInput.js
@@ -1,9 +1,30 @@
+import { act } from "react-dom/test-utils";
 import "./UserInput.css";
-import { useRef } from "react";
+import { useRef, useReducer } from "react";
 
 export default function UserInput(props) {
+  const nameFn = (state, action) => {
+    if (action.type === "TYPE")
+      return {
+        value: action.value,
+        hasFocus: state.hasFocus,
+      };
+    if (action.type === "FOCUS")
+      return {
+        value: state.value,
+        hasFocus: action.value,
+      };
+  };
   const inputName = useRef();
   const inputAge = useRef();
+  const [nameState, dispatchName] = useReducer(nameFn, {
+    value: "",
+    hasFocus: false,
+  });
+
+  const updateName = (event) => {
+    dispatchName({ type: "name", value: event.target.value });
+  };
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -13,11 +34,21 @@ export default function UserInput(props) {
     });
   };
 
+  const test = () => {
+    dispatchName({ type: "FOCUS", value: true });
+  };
+
   return (
     <div className="container">
       <form onSubmit={submitHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" ref={inputName} />
+        <input
+          type="text"
+          id="username"
+          onChange={updateName}
+          onBlur={test}
+          className={nameState.hasFocus ? "focus" : ""}
+        />
         <label htmlFor="age">Age (Years)</label>
         <input type="number" id="age" ref={inputAge} />
         <button type="submit">Add User</button>
